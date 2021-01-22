7d3555827ab1262b66ffd353879e6933e3ba8e79
TzupingWen <pw.rinya@gmail.com>
2021-01-22 11:51:23 +0800

    add 1/22

--- components/LatestNews/LatestNews.js
+++ components/LatestNews/LatestNews.js
@@ -1,47 +1,49 @@
 import React, {useState, useEffect} from 'react'
 import './LatestNews.scss'
 import { BsChevronRight } from "react-icons/bs"
+// import {Link} from 'react-router-dom'
 
 function LatestNews(props) {
-    // const [acNotifyData, setAcNotifyData] = useState([])
-    // const [orNotifyData, setOrNotifyData] = useState([])
-    // const [leNotifyData, setLeNotifyData] = useState([])
+    const [accountNotify, setAccountNotify] = useState([])
+    const [ordersNotify, setOrdersNotify] = useState([])
+    const [lessonNotify, setLessonNotify] = useState([])
 
-    // async function getMembers(){
-    //     try {
-    //         const response = await fetch(
-    //             'http://localhost:3001/members',
-    //             {
-    //                 method:'get',
-    //                 headers: {
-    //                     Accept: 'application/json',
-    //                     'Content-Type': 'application/json',
-    //                 },
-    //             }
-    //         )
-    //         if(response.ok){
-    //             const data = await response.json()
-    //             console.log('data',data) 
-    //             const datas = data[0].notifications_account
-    //             console.log('datas',datas)
-    //             const orders = data[0].notifications_orders
-    //             console.log('orders',orders)
-    //             const classsess = data[0].notifications_lesson
-    //             console.log('classsess',classsess)
-    //             console.log(data)
-    //             setAccountNotify(datas)
-    //             setOrdersNotify(orders)
-    //             setLessonNotify(classsess)
+    async function getMembers(){
+
+        try {
+            const response = await fetch(
+                'http://localhost:3001/members',
+                {
+                    method:'get',
+                    // headers: {
+                    //     Accept: 'application/json',
+                    //     'Content-Type': 'application/json',
+                    // },
+                }
+            )
+            if(response.ok){
+                const data = await response.json()
+                console.log('data',data) 
+                const datas = data[0].notifications_account[0]
+                console.log('datas',datas)
+                const orders = data[0].notifications_orders[0]
+                console.log('orders',orders)
+                const classsess = data[0].notifications_lesson[0]
+                console.log('classsess',classsess)
+                
+                setAccountNotify(datas)
+                setOrdersNotify(orders)
+                setLessonNotify(classsess)
                
-    //         } 
-    //     } catch(error) {
-    //         console.log('error',error)
-    //     }
-    // }
+            } 
+        } catch(error) {
+            console.log('error',error)
+        }
+    }
 
-    // useEffect(()=>{
-    //     getMembers()
-    // },[])
+    useEffect(()=>{
+        getMembers()
+    },[])
 
     return (
         <>
@@ -50,15 +52,15 @@ function LatestNews(props) {
                     <p>最新消息</p>
                 </div>
                 <div className="w-news-main">
-                    <p>【帳戶通知】您在2021年1月29日修改了個人檔案
+                    <p>{accountNotify.accountnotify_title}{accountNotify.accountnotify_content}
                     <BsChevronRight className="w-iconright" size="20" />
                     </p>
                     <hr/>
-                    <p>【訂單通知】您在2021年1月29日訂購了琴葉榕大型植栽
+                    <p>{ordersNotify.orderlistnotify_title}{ordersNotify.orderlistnotify_content}
                     <BsChevronRight className="w-iconright" size="20" />
                     </p>
                     <hr/>
-                    <p>【預約通知】您在2021年1月29日預約了仙人掌花圈手作課
+                    <p>{lessonNotify.lessonnotify_title}{lessonNotify.lessonnotify_content}
                     <BsChevronRight className="w-iconright" size="20" />
                     </p>
                 </div>
